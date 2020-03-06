0e70c80fe578ac93491c9f27e628751c6761e5ef
D3viii <devinsdevs@gmail.com>
2020-03-06 17:29:28 -0500

    adding Variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var letters = ["a,b,c,d,e,f,g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z"]
+var numbers = ["1,2,3,4,5,6,7,8,9,0"]
+var characters = ["!,@,#,$,%,&,*"]
+
+function generatePassword(){
+
+
+
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
